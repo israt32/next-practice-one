4fb402c772a3ca075efff35d3a4b658cfab2f5cc
Israt Jahan <israt5227@gmail.com>
2026-09-02 23:05:01 +0600

    lorem added

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -9,6 +9,7 @@ const PostsPage = async() => {
       <h2>all my posts are here</h2>
       <p>read the file here</p>
       <p>road map</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste animi pariatur sit incidunt maiores explicabo error, debitis alias expedita, ipsum rerum autem quibusdam fuga mollitia qui magnam placeat voluptatibus optio.</p>
       {
         posts.map(post => <div key={post.id}>
           <h1>this is new posts</h1>
